3573356cb33ef67a77c3720521fd99cec8c77134
ZamarroLopez <mariolopezelices@gmail.com>
2021-03-12 16:37:26 +0100

    Subida reestructuracion

--- appWeb/client/src/app/app.module.ts
+++ appWeb/client/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { FormsModule, NgForm } from '@angular/forms';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgForm
+
   ],
   providers: [],
   bootstrap: [AppComponent]
